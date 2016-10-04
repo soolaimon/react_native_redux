ffa21a1ad04db88c7beb425a0e2b34b6ec6ed742
soolaimon <davidicus21@gmail.com>
2016-10-03 19:56:02 -0600

    auth

---
+++ auth/src/app.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+export default class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCj57CDhJQK9SYN5Q5OlbF1N3NhNOyjns8',
+      authDomain: 'auth-3ae44.firebaseapp.com',
+      databaseURL: 'https://auth-3ae44.firebaseio.com',
+      storageBucket: 'auth-3ae44.appspot.com',
+      messagingSenderId: '719114069160'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+       return (
+         <Button onPress={() => firebase.auth().signOut()}>
+          Log Out
+         </Button>
+       );
+
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
